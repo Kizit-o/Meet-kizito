39285a74608cc556a6f666ee917fb7a421bc0e3f
Kizito <ngerechisom55@gmail.com>
2025-11-25 02:02:49 +0100

    Remove dist, docs, and gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Meet-kizito/',
-  server: {
-    host: true, // Allows network access (including ngrok)
-    port: 5173,
+  base: '/Meet-kizito/', // GitHub Pages repo base
+  build: {
+    outDir: 'dist',        // normal build folder
+    emptyOutDir: true,     // clears dist/ before each build
   },
 });
